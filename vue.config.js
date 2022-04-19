d9c2f775ee64c59839df6aa8de0066d2b7648ece
hd-david <mwewahoby@gmailcom>
2022-04-19 13:13:43 +0000

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  transpileDependencies: true,
+  publicPath: './',
+  configureWebpack: {
+    devServer: {
+      webSocketServer: false,
+      liveReload: false,
+      hot: false
+      }
+  }
+}
