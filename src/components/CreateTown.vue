7e38eecbd715fffef7afef3f8fa2c717e978e0b0
hd-david <mwewahoby@gmailcom>
2022-04-22 10:43:49 +0000

    updating createtown

--- src/components/CreateTown.vue
+++ src/components/CreateTown.vue
@@ -2,24 +2,24 @@
 <div> 
     <h3> {{msg}}</h3>
     <p> we help you drive your business with the modern technolody. </p>
-  <form >
+    <div class="alert alert-success" v-if="isSuccess">
+            Post Created Successfully
+        </div>
+  <form @submit.prevent='OnCreateTown' class="add-town">
     <div class="form-control">
-      <label>Town</label>
-      <!-- <input type="text" v-model="text" name="text" placeholder="name" />
-      <input type="text" v-model="text" name="text" placeholder="elevation" />
-      <input type="text" v-model="text" name="text" placeholder="grid reference" />
-      <input type="text" v-model="text" name="text" placeholder="longitude" />
-      <input type="text" v-model="text" name="text" placeholder="latitude" /> -->
-      <BasicInputForm
-      :placeholder="name"/>
-      <BasicInputForm
-      :placeholder="elevation"/>
-      <BasicInputForm
-      :placeholder="gridreference"/>
-      <BasicInputForm
-      :placeholder="longitude"/>
-      <BasicInputForm
-      :placeholder="latitude"/>
+      <label>task</label>
+      <input type="text" class ="form-control" v-model="text" name="text" placeholder="name" />
+      <input type="number" step="any" class ="form-control" v-model="text" name="text" placeholder="elevation" />
+      <input type="text" class ="form-control" v-model="text" name="text" placeholder="grid reference" />
+      <input type="number" step="any" class ="form-control"  v-model="text" name="text" placeholder="longitude" />
+      <input type="number" step="any" class ="form- control" v-model="text" name="text" placeholder="latitude" />
+      <input type="text" class ="form- control" v-model="text" name="text" placeholder="postcode_secto" />
+      <input type="text" class ="form- control" v-model="text" name="text" placeholder="nuts_region" />
+      <input type="number" step="any" class ="form- control" v-model="text" name="text" placeholder="northing" />
+      <input type="number" step="any" class ="form- control" v-model="text" name="text" placeholder="easting" />
+      <input type="text" class ="form- control" v-model="text" name="text" placeholder="town_type" />
+      <input type="text" class ="form- control" v-model="text" name="text" placeholder="local_government_area" />
+      
     </div>
     <input type="submit" value="submit" class="btn btn-block" />
   </form>
@@ -27,12 +27,50 @@
 </template>
 
 <script>
+import axios from 'axios';
 export default {
     name: 'CreateTown',
     props:{
         msg: String
-    }
-}
+    },
+
+    data(){ // the data expected from the form
+      return{
+        name: '',
+        elevation: '',
+        grid_reference: '',
+        longitude: '',
+        latitude: '',
+        postcode_secto:'',
+        nuts_region:'',
+        northing:'',
+        easting :'',
+        town_type:'',
+        local_government_area:'',
+        isSuccess: false,
+
+      
+      };
+      
+    },
+    methods:{
+      OnCreateTown(){ // when the submit button is clicked, the form data is sent to the backend
+        axios.post('https://david.darwinist.io/proxy/5000/town.json',{//using axios to make http requests
+          name: this.name, elevation: this.elevation, grid_reference: this.grid_reference, longitude: this.longitude,
+          latitude: this.latitude, postcode_sector: this.postcode_secto, nuts_region: this.nuts_region, northing: this.northing,
+          easting: this.easting, town_type: this.town_type, local_government_area: this.local_government_area
+          
+        })
+        .then((response) => {// checking response in the console
+                    this.isSuccess = true;
+                    console.log(response);
+                    this.$emit('towncreated');
+      });
+
+      },
+      
+    },
+};
 </script>
 
 <style scoped>
@@ -43,8 +81,8 @@ export default {
   display: block;
 }
 .form-control input {
-  width: 50%;
-  height: 40px;
+  width: 48%;
+  height: 20px;
   margin: 5px;
   padding: 3px 7px;
   font-size: 17px;
--- src/main.js
+++ src/main.js
@@ -1,9 +1,4 @@
-import axios from 'axios'
 import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
-
-axios
-  .get('https://david.darwinist.io/proxy/5000/town')
-  .then(response => (this.info = response.data.bpi))
