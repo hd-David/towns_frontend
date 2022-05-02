4032533c8020257fd09980c826ad9a535ee2ab74
ChantyHe90 <ceru@gmx.net>
2022-05-02 20:04:40 +0000

    implemented search

--- src/components/SearchTown.vue
+++ src/components/SearchTown.vue
@@ -1,8 +1,25 @@
 <template>
    <div class="container">
-     <button @click="SearchTown"> Search Town </button>
+    
 
-   </div>
+ 
+   <div id="town-search">
+
+  <div class="input-container">
+   
+     <input type="text" placeholder="Type a county" v-model="searchInputCounty"/>
+      <input type="text" placeholder="Type a town's name" v-model="searchInputName"/>
+      <button @click="SearchTown"> Search Town </button>
+  </div>
+ 
+<div class="columns medium-3" v-for="result in results" :key="result.value">
+
+      <p>
+        HELLO!!  {{ result }}.</p>
+
+</div>  
+  </div>
+</div>
 
 </template>
 
@@ -15,32 +32,55 @@ export default {
         },
   data(){
     return{
-      search: [],
+      searchInputCounty: [],
+      searchInputName: [],
+      results: []
     }
     
     },
   methods :{
-      SearchTown() {
+    SearchTown() {
     axios
       .get('https://chanty.darwinist.io/proxy/5000/town', {params: 
       {
-    "county": "Derbyshire", 
-    "name": "Ambaston"
+        // county: "Derbyshire"
+    "county": this.searchInputCounty, 
+    // name: "Ambergate",
+    "name": this.searchInputName
 }
       })
-      .then(response => {
-      console.log("hi data", 
-      response.data.easting,
-      response.data.local_government_area,
-      response.data.county,
-      response.data.name
-      ),
-      console.log("hi random", response.data)
-})
+     .then(response => {
+    this.results = response.data.county + response.data.name + response.data.nation
+    console.log("hi results", response.data.results),
+    console.log("hi county ", response.data.county),
+    console.log("hi name", response.data.name)
+    },
+)
  }
 }
 //  https//www.domain.com/page?key1=value1&key2=value2 
 // https://chanty.darwinist.io/proxy/5000/town?name=Derbyshire&county=Ambaston
+// https://chanty.darwinist.io/proxy/5000/town?county=derbyshire
 
 }
 </script>
+<style>
+.input-container {
+	border-radius: 5px;
+	background: #677482;
+	padding: 10px;
+}
+
+.input-container input {
+	border: none;
+	background: transparent;
+	color: white;
+	padding: 6px 15px;
+	font-size: 18px;
+}
+
+::placeholder { /* Chrome, Firefox, Opera, Safari 10.1+ */
+  color: #a6b0ba;
+  opacity: 1; /* Firefox */
+}
+</style>
